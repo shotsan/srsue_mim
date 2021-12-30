367c10875fd9a740caf88f18cc83c31aa7c22499
santosh <sivasantosh2@gmail.com>
2021-12-30 16:25:44 -0600

    error in the code for fixing priority, reverted changes

--- build/lib/include/srsran/build_info.h
+++ build/lib/include/srsran/build_info.h
@@ -43,7 +43,7 @@ extern "C" {
 #endif
 
 // the configured build options for srsRAN
-static char build_info[] = "commit 5275f3336 on branch master";
+static char build_info[] = "commit 9e6a5e789 on branch master";
 
 SRSRAN_API char* srsran_get_build_info() {
   return build_info;
